50b883fbc595cff5870910ef22ffda110af2e2eb
Keith Palmer-Hughes <8148322+Keifeh@users.noreply.github.com>
2023-11-20 17:47:08 +0000

    connect to psql

--- index.js
+++ index.js
@@ -2,8 +2,24 @@ const express = require('express')
 const app = express()
 const port = 3001
 
+const psqld_connect = require('./psqld-connect.js')
+
+app.use(express.json())
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+  next();
+});
+
 app.get('/', (req, res) => {
-  res.status(200).send('Hello World!');
+  psqld_connect.getPersons()
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
 })
 
 app.listen(port, () => {
